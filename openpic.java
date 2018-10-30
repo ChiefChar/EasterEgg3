1fca13c99ebf2731c0ad8f46794b1b36219abdf9
BuildTools <unconfigured@null.spigotmc.org>
2018-10-30 12:14:43 -0500

    OpenSOng unnecesary

--- openpic.java
+++ openpic.java
@@ -1,22 +1,23 @@
 import java.awt.Desktop;
 import java.io.File;
-
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 
 public class openpic {
 
-public static void main (String args[]) throws Exception
+    public static void main (String args[]) throws Exception
 
-{
+    {
+
+        Path filepath = Paths.get("./snp.jpg");
+        File f = new File ("C:\\Users\\charl\\Desktop\\Computer Science\\JavaProjects\\src\\snp.png");
+        Desktop d = Desktop.getDesktop();
+        d.open(f);
+        System.out.println("imageviewer open;");
 
 
-    File f = new File ("C:\\Users\\charl\\Desktop\\Computer Science\\JavaProjects\\src\\snp.png");
-    Desktop d = Desktop.getDesktop();
-    d.open(f);
-    System.out.println("imageviewer open;");
 
-
-
-}
+    }
 
 }
